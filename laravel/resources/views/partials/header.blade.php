e4584231f077a935272fe490ea90b8401ebe7cb6
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-07-05 12:02:48 +0200

    ADD header

--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -1,30 +1,59 @@
 <header class="mx-5 px-5">
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary px-5">
+    <nav class="navbar navbar-expand-lg">
+
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
+
+            <div>
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo">
+                </a>
+            </div>
+            
+            <div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CHARACTERS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COMICS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MOVIES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">GAMES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COLLECTIBLES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">VIDEOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FANS</a>
+                        </li>
+                    </ul>
+            </div>
             </div>
         </div>
     </nav>
 
 </header>
+
+<div class="jumbotron">
+
+    <img src="{{ asset('img/jumbotron.jpg') }}" alt="DC Logo">
+
+    <span class="tag">
+        CURRENT SERIES
+    </span>
+
+    
+
+
+</div>
